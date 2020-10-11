6be7376155a84b354b5e8f14092e68e648510a4f
Josiah Witheford <josiah.witheford@gmail.com>
2020-10-11 16:15:29 +1300

    Add support for CORS

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +126,5 @@ STATIC_URL = '/static/'
 ASSETS_PATH = 'assets/posts/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'assets/posts/')
+
+CORS_ORIGIN_ALLOW_ALL = True
